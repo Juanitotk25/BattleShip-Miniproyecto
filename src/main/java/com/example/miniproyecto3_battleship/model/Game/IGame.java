627f970656ee121e00803ff25deef159ae5f66b2
Juan David Lopez Vanegas <juan.lopez.vanegas@correounivalle.edu.co>
2025-05-14 18:28:29 -0500

    Creación de la interfaz IGame en el package Game, donde se nombran los metodos claves para el battleship game

---
+++ src/main/java/com/example/miniproyecto3_battleship/model/Game/IGame.java
@@ -0,0 +1,22 @@
+package com.example.miniproyecto3_battleship.model.Game;
+
+import com.example.miniproyecto3_battleship.model.Player.IPlayer;
+import com.example.miniproyecto3_battleship.model.Player.PlayerBot;
+import com.example.miniproyecto3_battleship.model.Player.PlayerPerson;
+
+import java.io.Serializable;
+
+public interface IGame extends Serializable {
+
+    //retorna el object que representa al bot
+    PlayerBot getPlayerBot();
+
+    //actualiza a nueva instancia de playerbot
+    void setPlayerBot(PlayerBot playerBot);
+
+    //retorna el object que representa la instancia de usuario
+    PlayerPerson getPlayerPerson();
+
+    //verifica si el jguador ha ganado y retorna true si ganó
+    Boolean verifyWinner(IPlayer player);
+}
