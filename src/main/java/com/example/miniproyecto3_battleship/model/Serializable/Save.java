27f78e814d4c61ad92618a11c1ae00c3dbaf0592
Juan David Lopez Vanegas <juan.lopez.vanegas@correounivalle.edu.co>
2025-05-13 21:31:22 -0500

    Se crea la clase Save para implementar el guardado de archivos

--- src/main/java/com/example/miniproyecto3_battleship/model/Serializable/Save.java
+++ src/main/java/com/example/miniproyecto3_battleship/model/Serializable/Save.java
@@ -1,4 +1,15 @@
 package com.example.miniproyecto3_battleship.model.Serializable;
 
-public class Save {
+import com.example.miniproyecto3_battleship.model.ships.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Save implements Serializable {
+    private ArrayList<int[]> shipPositions;
+
+    public Save(ArrayList<int[]> ship) {
+        this.shipPositions = ship;
+    }
+
 }
